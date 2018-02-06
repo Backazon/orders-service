7dfb667da91ea5b7a8db5afea89ff21854fb7c66
Chase Lee <chaseklee990@gmail.com>
2018-02-05 19:46:16 -0800

    (feat) Implement place order endpoint to insert data into DB

--- server/routers.js
+++ server/routers.js
@@ -4,13 +4,14 @@ const client = require('../database/index.js');
 const router = express.Router();
 
 client.connect(() => console.log('cassandra driver connected'));
+const table = process.env.TABLE_NAME || 'orders';
 
 router.get('/', (req, res) => {
   res.send('connected!');
 });
 
 router.get('/api/ordersByUser', async (req, res) => {
-  const query = 'SELECT * FROM orders WHERE userid=62693';
+  const query = `SELECT * FROM ${table} WHERE userid=62693`;
   try {
     const result = await client.execute(query);
     res.status(200).json(result.rows[0]);
@@ -20,16 +21,29 @@ router.get('/api/ordersByUser', async (req, res) => {
 });
 
 router.get('/api/ordersByDate', async (req, res) => {
-  const query = "SELECT * FROM orders WHERE date > '2017-01-07' ALLOW FILTERING";
+  const query = `SELECT userid, items, totalprice FROM ${table} WHERE date < '2017-01-07' AND userid > 1 AND userid < 100 ALLOW FILTERING`;
   try {
     const result = await client.execute(query);
-    res.status(200).json(result.rows[0]);
+    res.status(200).json(result.rows);
   } catch (error) {
     res.status(500).json(error.stack);
   }
 });
 
-// router.post('/api/placeOrder')
+router.post('/api/placeOrder', async (req, res) => {
+  const {
+    userid, date, items, orderid, purchasemethod, timestamp, totalprice,
+  } = req.body;
+  const query = `INSERT INTO ${table} (userid, date, items, orderid, purchasemethod, timestamp, totalprice) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  const params = [userid, date, items, orderid, purchasemethod, timestamp, totalprice];
+  try {
+    const result = await client.execute(query, params, { prepare: true });
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json(error.stack);
+  }
+});
+
 // router.post('/api/updateInventory')
 // router.post('/api/updateAnalytics)
 
