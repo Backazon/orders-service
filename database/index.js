12a6cbcb6955247826cf09ced591528c14963d00
Chase Lee <chaseklee990@gmail.com>
2018-02-05 16:08:58 -0800

    (feat) Add basic configuration ie env / yaml testing file (#4)

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({
   contactPoints: [process.env.CONTACT_POINT || '127.0.0.1'],
-  keyspace: 'backazon',
+  keyspace: process.env.KEYSPACE_NAME || 'backazon',
 });
 
 module.exports = client;
