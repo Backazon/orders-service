0d18e544c1eed2076b2c54a17bb5c3aea8506313
Chase Lee <chaseklee990@gmail.com>
2018-02-05 16:00:59 -0800

    (feat) Add basic configuration ie env / yaml testing file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
+const nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const compression = require('compression');
+const compression = require('compression');
 const router = require('./routers.js');
 
 const app = express();
@@ -8,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app
+  .use(compression())
   .use(bodyParser.json())
-  // .use(compression())
   .use(router)
   .listen(PORT, () => {
     console.log(`Listening on port ${PORT}!`);
