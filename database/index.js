5ce052078445b986323dc3c2436c2e8648185eb4
Chase Lee <chaseklee990@gmail.com>
2018-02-06 12:13:26 -0800

    (refactor) Modulize database queries out of server/index.js

--- database/index.js
+++ database/index.js
@@ -5,4 +5,36 @@ const client = new cassandra.Client({
   keyspace: process.env.KEYSPACE_NAME || 'backazon',
 });
 
-module.exports = client;
+client.connect(() => console.log('cassandra driver connected'));
+
+const table = process.env.TABLE_NAME || 'orders';
+
+// returns all the historical orders from a specific user id as an array of objects
+const ordersByUser = userid =>
+  client.execute(`SELECT * FROM ${table} WHERE userid=${userid}`).then(result => result.rows);
+
+// TODO: figure out what parameters
+// returns order items in an array for a specific date range
+const ordersByDate = () =>
+  client
+    .execute(`SELECT userid, items, totalprice FROM ${table} WHERE date < '2017-01-07' ALLOW FILTERING`)
+    .then(result => result.rows);
+
+// inserts an order into the cassandra database & takes in a formatted array as a parameter
+// [userid(int), date (new Date()), items(string), orderid(int), purchasemethod(string),
+// timestamp(bigint), totalprice(int)]
+const placeOrder = params =>
+  client
+    .execute(
+      `INSERT INTO ${table} (userid, date, items, orderid, purchasemethod, timestamp, totalprice) VALUES (?, ?, ?, ?, ?, ?, ?)`,
+      params,
+      { prepare: true },
+    )
+    .then(result => result);
+
+module.exports = {
+  client,
+  ordersByUser,
+  ordersByDate,
+  placeOrder,
+};
--- server/routers.js
+++ server/routers.js
@@ -1,30 +1,26 @@
 const express = require('express');
-const client = require('../database/index.js');
+
+const db = require('../database');
 
 const router = express.Router();
-
-client.connect(() => console.log('cassandra driver connected'));
-const table = process.env.TABLE_NAME || 'orders';
 
 router.get('/', (req, res) => {
   res.send('connected!');
 });
 
 router.get('/api/ordersByUser', async (req, res) => {
-  const query = `SELECT * FROM ${table} WHERE userid=62693`;
   try {
-    const result = await client.execute(query);
-    res.status(200).json(result.rows[0]);
+    const result = await db.ordersByUser(req.body.userid);
+    res.status(200).json(result);
   } catch (error) {
     res.status(500).json(error.stack);
   }
 });
 
 router.get('/api/ordersByDate', async (req, res) => {
-  const query = `SELECT userid, items, totalprice FROM ${table} WHERE date < '2017-01-07' AND userid > 1 AND userid < 100 ALLOW FILTERING`;
   try {
-    const result = await client.execute(query);
-    res.status(200).json(result.rows);
+    const result = await db.ordersByDate();
+    res.status(200).json(result);
   } catch (error) {
     res.status(500).json(error.stack);
   }
@@ -34,10 +30,9 @@ router.post('/api/placeOrder', async (req, res) => {
   const {
     userid, date, items, orderid, purchasemethod, timestamp, totalprice,
   } = req.body;
-  const query = `INSERT INTO ${table} (userid, date, items, orderid, purchasemethod, timestamp, totalprice) VALUES (?, ?, ?, ?, ?, ?, ?)`;
   const params = [userid, date, items, orderid, purchasemethod, timestamp, totalprice];
   try {
-    const result = await client.execute(query, params, { prepare: true });
+    const result = await db.placeOrder(params);
     res.status(201).json(result);
   } catch (error) {
     res.status(500).json(error.stack);
